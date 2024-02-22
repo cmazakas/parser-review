948ce07be51998678003a2baee404ca273bcae3b
Christian Mazakas <christian.mazakas@gmail.com>
2024-02-22 12:28:18 -0800

    Clean up parser

--- src/response_parser.cpp
+++ src/response_parser.cpp
@@ -1,33 +1,22 @@
+#include <boost/parser/error_handling_fwd.hpp>
 #include <http/response_parser.hpp>
 
+#include <boost/hana/tuple.hpp>
 #include <boost/parser/parser.hpp>
+
+namespace hana = boost::hana;
+namespace parser = boost::parser;
 
 #include <iostream>
 #include <ranges>
+
+using namespace hana::literals;
 
 namespace http {
 namespace {
 // HTTP-message = start-line CRLF *( field-line CRLF ) CRLF [
 //  message-body ]
 // start-line = request-line / status-line
-
-auto const on_major = [](auto &ctx) {
-  auto const major = _attr(ctx);
-  if (major < 1 || major > 9) {
-    _pass(ctx) = false;
-    return;
-  }
-  _globals(ctx).md_.version_.major_ = major;
-};
-
-auto const on_minor = [](auto &ctx) {
-  auto const minor = _attr(ctx);
-  if (minor < 0 || minor > 9) {
-    _pass(ctx) = false;
-    return;
-  }
-  _globals(ctx).md_.version_.minor_ = minor;
-};
 
 auto const on_status_code = [](auto &ctx) {
   auto status = _attr(ctx);
@@ -42,18 +31,30 @@ auto const on_status_code = [](auto &ctx) {
   md.status_line_end_ = subrange.end();
 };
 
+template <class> struct X;
+
 // HTTP-version = HTTP-name "/" DIGIT "." DIGIT
 // HTTP-name = %x48.54.54.50 ; HTTP
-auto const http_name = boost::parser::lit("HTTP");
+auto const on_version = [](auto &ctx) {
+  auto const &tup = _attr(ctx);
+  auto const major = tup[0_c];
+  auto const minor = tup[1_c];
+  if (major > 9 || minor > 9) {
+    _pass(ctx) = false;
+    return;
+  }
 
-boost::parser::rule<struct http_version> http_version = "http_version";
-auto const http_version_def = http_name >> "/" >>
-                              boost::parser::uint_[on_major] >> "." >>
-                              boost::parser::uint_[on_minor];
+  response::metadata &md = _globals(ctx).md_;
+  md.version_.major_ = major;
+  md.version_.minor_ = minor;
+};
+
+auto const http_name = parser::lit("HTTP");
+auto const http_version =
+    (http_name >> "/" >> parser::uint_ >> "." >> parser::uint_)[on_version];
 
 // status-code = 3DIGIT
-boost::parser::rule<struct status_code> status_code = "status_code";
-auto const status_code_def = boost::parser::uint_[on_status_code];
+auto const status_code = parser::uint_[on_status_code];
 
 // reason-phrase = 1*( HTAB / SP / VCHAR / obs-text )
 // VCHAR         = %x21-7E
@@ -74,16 +75,12 @@ auto const on_reason_phrase = [](auto &ctx) {
   ;
 };
 
-boost::parser::rule<struct reason_phrase> reason_phrase = "reason_phrase";
-auto const reason_phrase_def =
-    boost::parser::omit[+boost::parser::char_[on_reason_phrase]];
+auto const reason_phrase = parser::omit[+parser::char_[on_reason_phrase]];
 
 // status-line = HTTP-version SP status-code SP [ reason-phrase ]
-boost::parser::rule<struct status_line> status_line = "status_line";
-auto const status_line_def = http_version >> " " >> status_code >> " " >>
-                             -(reason_phrase);
+auto const status_line = http_version >> " " >> status_code >> " " >>
+                         -(reason_phrase);
 
-BOOST_PARSER_DEFINE_RULES(http_version, status_code, reason_phrase, status_line)
 } // namespace
 
 response parse_response(std::string_view &sv) {
@@ -93,8 +90,8 @@ response parse_response(std::string_view &sv) {
   auto begin = sv.begin();
   auto end = sv.end();
 
-  auto b = boost::parser::prefix_parse(
-      pos, end, boost::parser::with_globals(status_line, res));
+  auto b =
+      parser::prefix_parse(pos, end, parser::with_globals(status_line, res));
   (void)b;
 
   sv.remove_prefix(static_cast<std::size_t>(pos - begin));
