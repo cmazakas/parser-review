64e7d3ac9880bd420a1483045b8b45abb3c04144
Christian Mazakas <christian.mazakas@gmail.com>
2024-02-27 08:46:09 -0800

    Add small parser for discord user

---
+++ test/eisen_test.cpp
@@ -0,0 +1,113 @@
+#include <catch2/catch_test_macros.hpp>
+
+#include <boost/parser/parser.hpp>
+
+#include <iostream>
+#include <string_view>
+#include <vector>
+
+namespace p = boost::parser;
+
+struct metadata {
+  std::vector<std::string_view> msgs_;
+  int nlbraces_ = 0;
+  int nrbraces_ = 0;
+};
+
+TEST_CASE("for eisen") {
+
+  auto const on_lbrace = [](auto &ctx) {
+    metadata &md = _globals(ctx);
+    ++md.nlbraces_;
+  };
+
+  auto const on_rbrace = [](auto &ctx) {
+    metadata &md = _globals(ctx);
+    ++md.nrbraces_;
+  };
+
+  auto const on_content = [](auto &ctx) {
+    metadata &md = _globals(ctx);
+    md.msgs_.push_back(_attr(ctx));
+    std::cout << _attr(ctx) << std::endl;
+  };
+
+  auto const on_complete = [](auto &ctx) {
+    metadata &md = _globals(ctx);
+    if (md.nlbraces_ != md.nrbraces_) {
+      _pass(ctx) = false;
+    }
+  };
+
+  auto const lbrace = p::lit("{")[on_lbrace];
+  auto const rbrace = p::lit("}")[on_rbrace];
+
+  auto const ascii = p::char_(32, 126);
+  auto const comment =
+      p::lit("/*") >> *(+(ascii - '*') >> !p::char_('/')) >> "*/";
+  auto const content =
+      p::string_view[+((ascii - p::char_("{}")) - comment)][on_content];
+
+  auto const parser = (+lbrace >> +(content | comment) >> +rbrace)[on_complete];
+
+  {
+    std::string_view input = "{{{ rawr! }}}";
+    metadata md;
+    auto pos = input.begin();
+    auto result =
+        p::prefix_parse(pos, input.end(), p::with_globals(parser, md), p::ws);
+    CHECK(result);
+    CHECK(md.nlbraces_ == 3);
+    CHECK(md.nrbraces_ == 3);
+    CHECK(md.msgs_.at(0) == "rawr!");
+  }
+
+  {
+    std::string_view input = "{{{ rawr! }}";
+    metadata md;
+    auto pos = input.begin();
+    auto result =
+        p::prefix_parse(pos, input.end(), p::with_globals(parser, md), p::ws);
+    CHECK(!result);
+    CHECK(md.nlbraces_ == 3);
+    CHECK(md.nrbraces_ == 2);
+    CHECK(md.msgs_.at(0) == "rawr!");
+  }
+
+  {
+    std::string_view input = "{ rawr! }}";
+    metadata md;
+    auto pos = input.begin();
+    auto result =
+        p::prefix_parse(pos, input.end(), p::with_globals(parser, md), p::ws);
+    CHECK(!result);
+    CHECK(md.nlbraces_ == 1);
+    CHECK(md.nrbraces_ == 2);
+    CHECK(md.msgs_.at(0) == "rawr!");
+  }
+
+  {
+    std::string_view input = "{{{ ra{}wr! }}}";
+    metadata md;
+    auto pos = input.begin();
+    auto result =
+        p::prefix_parse(pos, input.end(), p::with_globals(parser, md), p::ws);
+    CHECK(!result);
+    CHECK(md.nlbraces_ == 3);
+    CHECK(md.nrbraces_ == 0);
+    CHECK(md.msgs_.at(0) == "ra");
+  }
+
+  {
+    std::string_view input = "{{{ ra/* {lmao}}}}}}} */wr! }}}";
+    metadata md;
+    auto pos = input.begin();
+    auto result =
+        p::prefix_parse(pos, input.end(), p::with_globals(parser, md), p::ws);
+    CHECK(result);
+    CHECK(md.nlbraces_ == 3);
+    CHECK(md.nrbraces_ == 3);
+    CHECK(md.msgs_.at(0) == "ra");
+    CHECK(md.msgs_.at(1) == "wr!");
+  }
+}
